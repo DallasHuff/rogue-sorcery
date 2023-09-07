58264a513f88b82fcf210759330148f59db25cb3
DallasHuff <dallashuff45@gmail.com>
2023-09-07 14:57:27 -0500

    coyote jumping and jump buffering. slight refactoring in other move scripts

--- rogue-sorcery/Assets/Scripts/Movement/Controllers/AIController.cs
+++ rogue-sorcery/Assets/Scripts/Movement/Controllers/AIController.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AIController", menuName = "InputController/AIController")]
 public class AIController : InputController
 {
+    [SerializeField] private Transform playerTrans;
+    [SerializeField] private Transform enemyTrans;
     public override float RetrieveMoveInput()
     {
-        return 1f;
+        //if (playerTrans.position.x > enemyTrans.position.x)
+        //{
+        //    return 1f;
+        //}
+        return -1f;
     }
 
     public override bool RetrieveJumpInput()
     {
         return true;
+    }
+
+    public override bool RetrieveJumpHoldInput()
+    {
+        return false;
     }
 }
